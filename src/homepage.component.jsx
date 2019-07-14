4400a6558a15134e83952c567ff5f99607a9248f
Shahul Hameed <beingshahul@gmail.com>
2019-07-14 17:30:56 +0530

    Create a Homepage component.

---
+++ src/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Homepage = () => (
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                
+            </div>
+        </div>
+    </div>
+)
+
